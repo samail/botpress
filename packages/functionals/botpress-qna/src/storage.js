153a263dbc305c0280db03a4384b2f8d8378a228
Eugene Mirotin <emirotin@gmail.com>
2018-05-22 14:40:30 +0500

    feat(qna): storage complete

--- packages/functionals/botpress-qna/src/storage.js
+++ packages/functionals/botpress-qna/src/storage.js
@@ -8,12 +8,46 @@ const safeId = (length = 10) => generate('1234567890abcdefghijklmnopqrsuvwxyz', 
 
 const slugify = s => (s || '').toLowerCase().replace(/[^a-z0-9]/g, '_')
 
-const getQuestionId = ({ questions }) => `${safeId()}_${slugify(questions[0]).substring(0, 50)}`
+const getQuestionId = ({ questions }) =>
+  `${safeId()}_${slugify(questions[0])
+    .replace(/^_+/, '')
+    .substring(0, 50)
+    .replace(/_+$/, '')}`
 
 export default class Storage {
-  async getQuestions() {}
+  constructor({ bp, config, provider }) {
+    this.ghost = bp.ghostManager
+    this.qnaDir = config.qnaDir
+  }
+
+  async initializeGhost() {
+    mkdirp.sync(path.resolve(this.projectDir, this.qnaDir))
+    await this.ghost.addRootFolder(this.qnaDir, { filesGlob: '**/*.json' })
+  }
+
   async saveQuestion(id, data) {
     id = id || getQuestionId(data)
+    await this.ghost.upsertFile(this.qnaDir, `${id}.json`, JSON.stringify({ id, data }, null, 2))
   }
-  async deleteQuestion(id) {}
+
+  async getQuestion(id) {
+    let filename
+    if (typeof id === 'string') {
+      filename || `${id}.json`
+    } else {
+      // opts object
+      filename = id.filename
+    }
+    const data = await this.ghost.readFile(this.qnaDir, filename)
+    return JSON.parse(data)
+  }
+
+  async getQuestions() {
+    const questions = await this.ghost.directoryListing(this.intentsDir, '.json')
+    return Promise.map(questions, question => this.getQuestion({ filename: question }))
+  }
+
+  async deleteQuestion(id) {
+    await this.ghost.deleteFile(this.intentsDir, `${id}.json`)
+  }
 }
